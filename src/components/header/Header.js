a2e78aed2bea78a0ff6e286d8cecf5ec5e863f48
DaniloPath <danilo.castro.engcivil@gmail.com>
2020-06-29 12:00:08 -0300

    Estilizando o Header do APP

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { formatNumber } from '../../helpers/formatHelpers'
 
+import css from './header.module.css'
+
 export default class Header extends Component {
     handleInputChange = (event) => {
         const newText = event.target.value
@@ -11,10 +13,10 @@ export default class Header extends Component {
     render() {
         const {filter, countryCount, totalPopulation}=this.props
         return (
-            <div>
+            <div className={css.flexRow}>
                 <input type="text" value={filter} onChange={this.handleInputChange} />|
-                <span>Paíse: {countryCount} </span>|                
-                <span>População: {formatNumber(totalPopulation)} </span>                
+                <span className={css.countries}> Paíse: <strong>{countryCount}</strong> </span>|                
+                <span className={css.population}>População: <strong>{formatNumber(totalPopulation)}</strong> </span>                
             </div>
         )
     }
